563057aac8f9b2664998e1b0e5a8b98bd3ede22f
Vladimir Infante <infantevladimir@gmail.com>
2024-07-18 12:19:16 -0500

    feat: added basic tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-bg": "#0E100F",
+        "primary-lila": "#A98AFF",
+        "primary-blue": "#17d1f1",
+        "secondary-blue": "#18A0FB",
+        bline: "rgb(50 50 40)",
+      },
+      dropShadow: {
+        dark: "0 -10px 20px rgb(23, 209, 241,0.3)",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+        },
+        screens: {
+          "2xl": "1300px",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
