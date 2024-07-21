7875f57043708d4123b1153903af43eabfae5217
Vladimir Infante <infantevladimir@gmail.com>
2024-07-20 19:12:06 -0500

    feat: added barrels for components primitives and composites

---
+++ src/components/composites/index.ts
@@ -0,0 +1,6 @@
+export * from "./animated-button-container/AnimatedButtonContainer";
+export * from "./core-service-card/CoreServiceCard";
+export * from "./lines-canvas/LinesCanvas";
+export * from "./skills-slider/SkillsSlider";
+export * from "./navbar/Navbar";
+export * from "./project-rows-container/ProjectRowsContainer";
---
+++ src/components/primitives/index.ts
@@ -0,0 +1,4 @@
+export { default as AnimatedBoxLink } from "./animated-box-link/AnimatedBoxLink.astro";
+export * from "./pulsating-dot/PulsatingDot";
+export * from "./text/Text";
+export * from "./tooltip/Tooltip";
