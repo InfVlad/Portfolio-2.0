8706a80f07bab18d5729745dfe5009a0a5289cd9
Vladimir Infante <infantevladimir@gmail.com>
2024-07-20 19:07:45 -0500

    feat: added cards for services offered

---
+++ src/components/composites/core-service-card/CoreServiceCard.tsx
@@ -0,0 +1,33 @@
+import { Text } from "~/components/primitives";
+import { cn } from "~/utils/cn";
+import type { CoreService } from "~/types/coreService";
+
+interface Props extends CoreService {
+  index: number;
+}
+
+export const CoreServiceCard = ({ title, description, Icon, index }: Props) => {
+  return (
+    <article
+      className={cn(
+        "autoShowRight flex h-[21.875rem] flex-1 flex-col rounded-md border border-bline bg-card p-10 pl-6 pt-36",
+        index % 2 === 1 ? "text-primary-lila" : "text-primary-blue",
+      )}
+      style={{
+        "--delay": `${0.2 * index}s`,
+        "--viewEnd": `${70 - 8 * index}%`,
+        "--viewStart": `${20 + 8 * index}%`,
+      }}
+    >
+      <div className="mb-4 h-fit w-full">
+        <Icon size={50} />
+      </div>
+      <Text element="h4" className="mb-3 text-lg font-semibold text-inherit">
+        {title}
+      </Text>
+      <Text element="p" className="text-white">
+        {description}
+      </Text>
+    </article>
+  );
+};
