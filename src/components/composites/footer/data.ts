c2cd6907728da2a1cbea584ecf1d0093c047addc
Vladimir Infante <infantevladimir@gmail.com>
2024-09-02 11:05:08 -0500

    feat: added data for footer

---
+++ src/components/composites/footer/data.ts
@@ -0,0 +1,31 @@
+import { GITHUB, LINKEDIN } from "~/data/socialsLinks";
+
+export const exploreLinks = [
+  {
+    content: "Home",
+    href: "/",
+  },
+  {
+    content: "Projects",
+    href: "/projects",
+  },
+  {
+    content: "About Me",
+    href: "/about",
+  },
+  {
+    content: "Contact",
+    href: "/contact",
+  },
+];
+
+export const socialLinks = [
+  {
+    content: "Linkedin",
+    href: LINKEDIN,
+  },
+  {
+    content: "Github",
+    href: GITHUB,
+  },
+];
