d24e2dce779006a2662b90e46cb3a32e77fcd3a2
Vladimir Infante <infantevladimir@gmail.com>
2024-09-02 13:46:33 -0500

    feat: updated animated button in hero

--- src/components/composites/animated-button-container/AnimatedButtonContainer.tsx
+++ src/components/composites/animated-button-container/AnimatedButtonContainer.tsx
@@ -3,11 +3,32 @@ import { cn } from "~/utils/cn";
 type Props = {
   children: ComponentChildren;
   className?: string;
+  isLink: boolean;
+  href?: string;
 };
 
-export const AnimatedButtonContainer = ({ children, className }: Props) => {
+export const AnimatedButtonContainer = ({
+  children,
+  className,
+  isLink,
+  href,
+}: Props) => {
+  if (isLink) {
+    return (
+      <a
+        href={href}
+        className={cn(
+          "group relative mb-6 flex cursor-pointer overflow-hidden rounded-3xl border border-bline bg-primary-bg px-4 py-3 font-semibold text-white",
+          className,
+        )}
+      >
+        <div className="z-10 flex items-center gap-x-3">{children}</div>
+        <div className="absolute left-0 top-[150%] z-[1] h-[150%] w-full rounded-[50%] bg-secondary-blue transition-all duration-500 ease-in-out group-hover:top-[20%] group-hover:scale-150"></div>
+      </a>
+    );
+  }
   return (
-    <div
+    <button
       className={cn(
         "group relative mb-6 flex cursor-pointer overflow-hidden rounded-3xl border border-bline bg-primary-bg px-4 py-3 font-semibold text-white",
         className,
@@ -15,6 +36,6 @@ export const AnimatedButtonContainer = ({ children, className }: Props) => {
     >
       <div className="z-10 flex items-center gap-x-3">{children}</div>
       <div className="absolute left-0 top-[150%] z-[1] h-[150%] w-full rounded-[50%] bg-secondary-blue transition-all duration-500 ease-in-out group-hover:top-[20%] group-hover:scale-150"></div>
-    </div>
+    </button>
   );
 };
