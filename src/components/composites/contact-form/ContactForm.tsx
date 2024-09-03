55d22eb64870c1cf087ca696fdab20fa74251d95
Vladimir Infante <infantevladimir@gmail.com>
2024-09-03 12:45:11 -0500

    test: updated contact form, but should fail deployment

--- src/components/composites/contact-form/ContactForm.tsx
+++ src/components/composites/contact-form/ContactForm.tsx
@@ -1,11 +1,14 @@
 import { useState } from "preact/hooks";
+import type { ComponentChildren } from "preact";
+import type { JSX } from "preact";
 import { FormInput } from "../form-input/FormInput";
+import { Toast } from "../../primitives/toast/Toast";
 import { AnimatedButtonContainer } from "../animated-button-container/AnimatedButtonContainer";
 import { inputsData } from "./data";
-import type { JSX } from "preact";
+import { useToast } from "~/hooks/useToast";
 import type { InputsList } from "./types";
 
-export const ContactForm = () => {
+export const ContactForm = ({ children }: { children: ComponentChildren }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [values, setValues] = useState<Record<InputsList, string>>({
     name: "",
@@ -13,12 +16,15 @@ export const ContactForm = () => {
     subject: "",
     message: "",
   });
+  const { showToast, message, isSuccess } = useToast();
 
   const handleSubmit = (e: JSX.TargetedSubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
     setIsLoading(true);
-    console.log(values);
-    setTimeout(() => setIsLoading(false), 400);
+    setTimeout(() => {
+      setIsLoading(false);
+      showToast("An error occured", false, {});
+    }, 400);
   };
 
   const onChange = (e: JSX.TargetedEvent<HTMLInputElement, Event>) => {
@@ -53,9 +59,10 @@ export const ContactForm = () => {
       </fieldset>
       <button type="submit">
         <AnimatedButtonContainer isLink={false} className="px-12">
-          Submit
+          {isLoading ? children : "Submit"}
         </AnimatedButtonContainer>
       </button>
+      {message && <Toast message={message} isSuccess={isSuccess} />}
     </form>
   );
 };
