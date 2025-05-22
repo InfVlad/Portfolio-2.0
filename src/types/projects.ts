7aab74db30ddc45b3ade317f6389cefe18603a18
Vladimir Infante <infantevladimir@gmail.com>
2025-05-22 16:31:20 -0500

    feat: updated project types

--- src/types/projects.ts
+++ src/types/projects.ts
@@ -1,9 +1,14 @@
+import type { ImageMetadata } from "astro";
 export interface SimplifiedProject {
   title: string;
   year: string;
   rol: string;
   technologies: string;
-  url: string;
+  webUrl?: string;
+  codeUrl?: string;
   slug: string;
   description: string;
+  bulletPoints: string[];
+  imageSrc: ImageMetadata;
+  imageAlt: string;
 }
