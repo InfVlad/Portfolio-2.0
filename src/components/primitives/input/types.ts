40a9353d15b7b303d52d1dbc6150d96e3ef9d6db
Vladimir Infante <infantevladimir@gmail.com>
2025-05-22 12:03:23 -0500

    feat: updated input types to more accurate definitions

--- src/components/primitives/input/types.ts
+++ src/components/primitives/input/types.ts
@@ -1,7 +1,8 @@
-import type { HTMLAttributes } from "preact/compat";
+import type { InputHTMLAttributes } from "preact/compat";
+
 import type { inputVariants } from "./styles";
 import type { VariantProps } from "class-variance-authority";
 
 export interface InputProps
-  extends HTMLAttributes<HTMLInputElement>,
+  extends InputHTMLAttributes<HTMLInputElement>,
     VariantProps<typeof inputVariants> {}
