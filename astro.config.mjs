4bb43e129c92f2a79b7d4d31fe762bd5cfa0dc91
Vladimir Infante <infantevladimir@gmail.com>
2025-05-24 22:27:34 -0500

    config: added sitemap config to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import preact from "@astrojs/preact";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()]
+  site: "https://portfolio.vladinfante.com",
+  integrations: [tailwind(), preact(), sitemap()],
 });
